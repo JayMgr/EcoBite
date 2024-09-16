1c2399a7b00b2f768274a8c4410e626bd9be628c
Jacus <Jacus@WARONPC>
2024-09-16 11:01:27 +0545

    Add project files.

---
+++ EcoBiteWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EcoBiteWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoBiteWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Fooditem> Fooditems { get; set; }
+    }
+}
---
+++ EcoBiteWeb/Models/Fooditem.cs
@@ -0,0 +1,12 @@
+﻿namespace EcoBiteWeb.Models
+{
+    public class Fooditem
+    {
+        public int FooditemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string ExpirationDate { get; set; }
+        
+    }
+}

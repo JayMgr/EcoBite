0883ce36501b7ae678ca982dd529623a0d4fc1cd
Jacus Khamcha <jacus772@gmail.com>
2024-10-01 09:41:03 +0545

    Consloe.Writeline Modified

--- EcoBiteWeb/Controllers/FooditemController.cs
+++ EcoBiteWeb/Controllers/FooditemController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcoBiteWeb.Data;
+using EcoBiteWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcoBiteWeb.Controllers
 {
     public class FooditemController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public FooditemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Fooditem> objFooditemList = _db.Fooditems.ToList();
+            return View(objFooditemList);
         }
     }
 }
